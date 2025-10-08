67b88973994d803ea8a14a26ebac0902e28109cd
DaandeMaat <daandemaat@gmail.com>
2025-10-08 23:02:35 +0200

    ORM modellen gemaakt voor AanvoerderDB en Koper DB

---
+++ WebProject_klas3_groep4/models/AanvoerderDB.cs
@@ -0,0 +1,19 @@
+﻿namespace WebProject_klas3_groep4.models
+{
+    public class AanvoerderDB
+    {
+        public int ID { get; set; }
+
+        public string KvkNummer { get; set; }
+
+        public string NaamVanBedrijf { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string Adres { get; set; }
+
+        public string BedrijfTelefoonnummer { get; set; }
+
+        public string BedrijfEmail { get; set; }
+    }
+}
---
+++ WebProject_klas3_groep4/models/KoperDB.cs
@@ -0,0 +1,13 @@
+﻿namespace WebProject_klas3_groep4.models
+{
+    public class KoperDB
+    {
+        public int ID { get; set; }
+
+        public string BankGegevens { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string Adres { get; set; }
+    }
+}
