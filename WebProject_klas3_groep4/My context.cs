7e24285ed35a49166196bcf169fe2825b38c0eb8
Haijin Hu <22181946@student.hhs.nl>
2025-10-08 19:33:35 +0200

    veranderingen aan dingen

---
+++ WebProject_klas3_groep4/My context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace WebProject_klas3_groep4
+{
+    public class MyContext : DbContext​
+    {​
+
+ protected override void OnConfiguring(DbContextOptionsBuilder b) => b.UseSqlite("Data Source=database.db");​
+ // b.UseSqlServer("Connection string") //Je kan de keuze van de database ook in de startup van de app plaatsen
+
+ protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Studenten { get; set; }​ 
+}
+
+}
--- WebProject_klas3_groep4/models/VeilingmeesterDB.cs
+++ WebProject_klas3_groep4/models/VeilingmeesterDB.cs
@@ -1,8 +1,10 @@
-﻿namespace WebProject_klas3_groep4
+﻿using WebProject_klas3_groep4.models;
+
+namespace WebProject_klas3_groep4
 {
-    public class VeilingmeesterDB
+    public class VeilingmeesterDB : UserDB
     {
-        public int ID { get; set; }
         public String VeilingVestiging { get; set; }
+        public List<VeilingDB> Veilingen { get; set; }
     }
 }
