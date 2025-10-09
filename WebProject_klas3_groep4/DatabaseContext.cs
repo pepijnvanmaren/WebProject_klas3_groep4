10ab811f6033d83b2db4ed66cd8873061afd39d9
pepijnvanmaren <24112879@student.hhs.nl>
2025-10-09 15:11:07 +0200

    swagger en db gefixt

---
+++ WebProject_klas3_groep4/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProject_klas3_groep4.models;
+
+namespace WebProject_klas3_groep4
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VeilingmeesterDB> Veilingmeesters { get; set; }
+        public DbSet<GebruikerDB> Gebruikers { get; set; }
+        public DbSet<VeilingDB> Veilingen { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=database.db");
+            }
+        }
+    }
+}
