a44a0c8ef9a81227e39f42655556a3e1ab1fd235
Haijin Hu <haijinhulol@gmail.com>
2025-10-09 12:17:43 +0200

    sqlite gefixed

--- WebProject_klas3_groep4/My context.cs
+++ WebProject_klas3_groep4/My context.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Reflection.Emit;
+
 
 namespace WebProject_klas3_groep4
 {
     public class MyContext : DbContext​
+    
     {​
+        public DbSet<VeilingmeesterDB>Veilingmeesteers { get; set; }
+        public DbSet<VeilingDB>Veilingen {  get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder b) => b.UseSqlite("Data Source=database.db");​
         public DbSet<VeilingmeesterDB> Veilingmeesters { get; set; }​ 
-}
+    }
 
 }
