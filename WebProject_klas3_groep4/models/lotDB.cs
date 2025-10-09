7601f5f8f1787ae43df9ddc54549aef519db1a47
pepijnvanmaren <24112879@student.hhs.nl>
2025-10-09 10:11:34 +0200

    ef core files

---
+++ WebProject_klas3_groep4/models/lotDB.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WebProject_klas3_groep4.models
+{
+    public class LotDB
+    {
+        public class Lot
+        {
+            public int LotID { get; set; }
+            public int ProductID { get; set; }
+            public Product? Product { get; set; }
+
+            public int VeilingID { get; set; }
+            public Veiling? Veiling { get; set; }
+
+            public DateTime? GebeurtenisDatum { get; set; }
+            public string? GewensteLocatie { get; set; }
+
+            public ICollection<Bod>? Biedingen { get; set; }
+        }
+    }
+}
+
---
+++ WebProject_klas3_groep4/models/productDB.cs
@@ -0,0 +1,18 @@
+﻿namespace WebProject_klas3_groep4.models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string? Naam { get; set; }
+        public string? Foto { get; set; }
+        public string? Beschrijving { get; set; }
+
+        public int AanvoerderID { get; set; }
+        public Aanvoerder? Aanvoerder { get; set; }
+
+        public ProductSpecificaties? Specificaties { get; set; }
+
+        public ICollection<Lot>? Lots { get; set; }
+    }
+}
+
