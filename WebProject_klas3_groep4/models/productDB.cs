485a4d9ecff76ea168cf8ef608ebb3116eb55b56
Haijin Hu <haijinhulol@gmail.com>
2025-10-09 12:41:20 +0200

    modellen gefixed, foreign keys

--- WebProject_klas3_groep4/models/productDB.cs
+++ WebProject_klas3_groep4/models/productDB.cs
@@ -2,17 +2,11 @@
 {
     public class Product
     {
-        public int ProductID { get; set; }
+        public int ID { get; set; }
         public string? Naam { get; set; }
         public string? Foto { get; set; }
         public string? Beschrijving { get; set; }
-
-        public int AanvoerderID { get; set; }
-        public Aanvoerder? Aanvoerder { get; set; }
-
-        public ProductSpecificaties? Specificaties { get; set; }
-
-        public ICollection<Lot>? Lots { get; set; }
+        public List<LotDB> Lists { get; set; }
     }
 }
 
