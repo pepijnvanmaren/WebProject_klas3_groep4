10a4eb33d8cb69680beb236e1cd3f569b37ed3d0
MattyJutte <mattyjutte07@gmail.com>
2025-10-28 11:23:06 +0100

    Navbar mooier gemaakt

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,10 +10,10 @@ function Navbar() {
             <img src={Trees} alt="tree picture" className="tree-picture" />
             <nav className="navbar">
                 <div className="nav-content">
-                    <img src={Logo} alt="Royale Flora" className="nav-logo" />
+                    <li><a href="#home"><img src={Logo} alt="Royale Flora" className="nav-logo" /></a></li>
                     <ul className="nav-links">
-                        <li><a href="#home">Home</a></li>
                         <li><a href="#about">Over ons</a></li>
+                        <li><a href="#logOut">Uitloggen</a></li>
                         <li><a href="#contact"><img src={UserIcon} alt="Royale Flora" className="nav-userIcon" /></a></li>
                     </ul>
                 </div>
