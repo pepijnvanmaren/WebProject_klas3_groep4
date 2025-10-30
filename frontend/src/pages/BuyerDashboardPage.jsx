41896fcb3f8eb2340c5eaad8f3b9b415e536f82b
DaandeMaat <daandemaat@gmail.com>
2025-10-30 12:57:12 +0100

    Dashboard.css aangepast zodat Dashboard zwart is inplaats van grijs en in Seller en Buyeer Dashboard export default function aangepast.

--- frontend/src/pages/BuyerDashboardPage.jsx
+++ frontend/src/pages/BuyerDashboardPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar.jsx";
 import BuyerDashboard from "../components/BuyerDashboard.jsx";
 
-export default function LoginPage() {
+export default function BuyerDashboardPage() {
     return (
         <>
             <Navbar />
