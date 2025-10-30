54648dac522ede8c840798ad88eac4fca244872b
MattyJutte <mattyjutte07@gmail.com>
2025-10-30 10:59:06 +0100

    Werkende versie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import SellerDashboardPage from "./pages/SellerDashboardPage";
-import BuyerDashboardPage from "./pages/BuyerDashboardPage";
 
 function App() {
     return (
@@ -12,7 +11,6 @@ function App() {
                 <Route path="/registreren" element={<RegisterPage />} />
                 <Route path="/inloggen" element={<LoginPage />} />
                 <Route path="/VerkoperDashboard" element={<SellerDashboardPage />} />
-                <Route path="/KoperDashboard" element={<BuyerDashboardPage />} />
                 <Route path="/" element={<IndexPage />} />
             </Routes>
         </BrowserRouter>
--- frontend/src/pages/BuyerDashboardPage.jsx
+++ frontend/src/pages/BuyerDashboardPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
-import BuyerDashboard from "../components/BuyerDashboard.jsx";
 import Index from "../components/Index.jsx";
 
 export default function LoginPage() {
