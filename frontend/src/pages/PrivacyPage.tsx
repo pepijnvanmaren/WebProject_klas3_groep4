4db203d3515604ed38f8db32b1efaab72e33cc8a
MattyJutte <mattyjutte07@gmail.com>
2025-11-13 12:01:02 +0100

    Footer toegevoegd en een privacy verklaring aangemaakt.

---
+++ frontend/src/pages/PrivacyPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar.js";
+import Privacy from "../components/Privacy.js";
+import Footer from "../components/Footer.jsx";
+export default function PrivacyPage() {
+    return (
+        <>
+            <Navbar/>
+            <Privacy/>
+            <Footer/>
+        </>
+    );
+}
