74a7144dce223be1ea707b6a4f56ae5a69c25f08
pepijnvanmaren <24112879@student.hhs.nl>
2025-11-27 13:25:42 +0100

    frontend fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import Logo from "../assets/RoyaleFloraLogo.svg";
 import Trees from "../assets/treePicture.png";
 import UserIcon from "../assets/userIcon3.png";
-    
+
 function Navbar() {
+    // Auth state -- heeft nog code nodig voor authenticatie.
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <header className="header-container">
             <img src={Trees} alt="tree picture" className="tree-picture" />
             <nav className="navbar">
                 <div className="nav-content">
-                    <li><a href="#home"><img src={Logo} alt="Royale Flora" className="nav-logo" /></a></li>
+                    <li>
+                        <Link to="/">
+                            <img src={Logo} alt="Royale Flora" className="nav-logo" />
+                        </Link>
+                    </li>
+
                     <ul className="nav-links">
-                        <li><a href="#about">Over ons</a></li>
-                        <Link to="/inloggen">Uitloggen</Link>
-                        <Link to="/AccountInfo"><img src={UserIcon} alt="Royale Flora" className="nav-userIcon" /></Link>
+
+                        <Link to="/inloggen">
+                            {isLoggedIn ? "Uitloggen" : "Inloggen"}
+                        </Link>
+
+                        {!isLoggedIn && (
+                            <Link to="/registreren">Registreren</Link>
+                        )}
+
+                        {isLoggedIn && (
+                            <Link to="/AccountInfo">
+                                <img
+                                    src={UserIcon}
+                                    alt="Royale Flora"
+                                    className="nav-userIcon"
+                                />
+                            </Link>
+                        )}
                     </ul>
                 </div>
             </nav>
