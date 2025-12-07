24dba1c2e44b3e61889a2c5788984ec59a1d0cf0
DaandeMaat <daandemaat@gmail.com>
2025-12-07 22:37:23 +0100

    Benodigd aantal test gemaakt en werkende

---
+++ WebProject_klas3_groep4.Tests/Infrastructure/TestAuthFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+public class TestAuthFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddAuthentication("Test")
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                    "Test", options => { });
+        });
+    }
+}
---
+++ WebProject_klas3_groep4.Tests/Infrastructure/WebAppFactory.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebProject_klas3_groep4;
+using WebProject_klas3_groep4.Tests;
+
+namespace WebProject_klas3_groep4.Tests.Infrastructure
+{
+    public class WebAppFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Verwijder de echte database voor de test
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<DatabaseContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                // Voeg InMemory database toe voor testen
+                services.AddDbContext<DatabaseContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDB");
+                });
+
+                services.AddAuthentication("Test")
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                        "Test", options => { });
+
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy("TestPolicy", policy =>
+                    {
+                        policy.AuthenticationSchemes.Add("Test");
+                        policy.RequireAuthenticatedUser();
+                    });
+                });
+
+                // Build provider en seed testdata
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+                    db.Database.EnsureCreated();
+
+                }
+            });
+        }
+    }
+}
